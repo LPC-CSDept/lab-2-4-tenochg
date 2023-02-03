7edd00d01576305c91054a1b4ba484be56fe83aa
tenochg <yomoozuki@gmail.com>
2023-02-02 17:11:20 -0800

    Lab 4

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    str = 'Python Programming'
+    sub1 = str[:6]
+    sub2 = str[7:]
+    print(sub1)
+    print(sub2)
+    print(sub2 +sub1)
     pass
 
 
